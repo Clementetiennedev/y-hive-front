871d3950b627cbf5f208f350a2dd4e5f69e0ecae
clementSIO <116883467+Clementetiennedev@users.noreply.github.com>
2025-02-04 10:10:38 +0100

    ADD navbar, without routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavMainComponent } from "./components/feature-navbar/nav-main/nav-main.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavMainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { NavMainComponent } from './components/feature-navbar/nav-main/nav-main.component';
 
 export const routes: Routes = [
-    { path: '', component: NavMainComponent }
+    // Route example
+    //{ path: 'home', component: nomComponent},  
 ];
