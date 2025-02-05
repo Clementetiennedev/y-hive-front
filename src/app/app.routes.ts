0e179e7bb65ab36c9be2723acea7faab330aba27
MatteoDinville <matteo.dinville@ynov.com>
2025-02-05 15:39:26 +0100

    Fix import and remove comment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,10 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { LandingPageComponent } from './components/feature-home-page/landing-page/landing-page.component';
+import { LandingPageComponent } from './components/feature-landing-page/landing-page/landing-page.component';
+
 
 export const routes: Routes = [
-    // Route example
-    //{ path: 'home', component: nomComponent},
     { path: '', component: LandingPageComponent},
     { path: 'landing', component: LandingPageComponent},
     { path: 'login', component: LoginComponent},
