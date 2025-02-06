0102c5810a50edecb283586e13cefcf881e15f7b
clementSIO <116883467+Clementetiennedev@users.noreply.github.com>
2025-02-06 15:13:10 +0100

    resolve

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,15 @@ import { RegisterComponent } from './auth/register/register.component';
 import { LandingPageComponent } from './components/feature-landing-page/landing-page/landing-page.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AboutUsComponent } from './components/feature-about-us/about-us/about-us.component';
 
 
 export const routes: Routes = [
-    { path: '', component: LandingPageComponent },
-    { path: 'home', component: HomeComponent },
-    { path: 'landing', component: LandingPageComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
+    { path: '', component: LandingPageComponent},
+    { path: 'home', component: HomeComponent},
+    { path: 'landing', component: LandingPageComponent},
+    { path: 'login', component: LoginComponent},
+    { path: 'register', component: RegisterComponent},
+    { path: 'about-us', component: AboutUsComponent},
     { path: 'profile', component: ProfileComponent }
 ];
