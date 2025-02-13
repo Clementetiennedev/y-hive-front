c3808c5f0b6672fabddd0851f7672751873ee71b
clementSIO <116883467+Clementetiennedev@users.noreply.github.com>
2025-02-13 10:30:48 +0100

    ADDconnection beetween navbar button and body section

--- src/app/components/nav-main/nav-main.component.ts
+++ src/app/components/nav-main/nav-main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 
+
 @Component({
   selector: 'app-nav-main',
-  imports: [RouterModule],
+  imports: [RouterModule, ],
   templateUrl: './nav-main.component.html',
   styleUrls: ['./nav-main.component.scss']
 })
@@ -14,6 +15,14 @@ export class NavMainComponent {
 
   constructor(private router: Router) {}
 
+   scrollToSection(sectionId: string) {
+    this.router.navigate([], { fragment: sectionId }).then(() => {
+      const element = document.getElementById(sectionId);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    });
+  }
   // Méthode pour alterner l'état du dropdown
   toggleDropdown() {
     this.isDropdownOpen = !this.isDropdownOpen;
